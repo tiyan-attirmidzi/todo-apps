d9033051f7e20ac1723a7b84bf467065cb0c2ec5
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2020-09-15 20:32:26 +0800

    add Collection UI Element with DOM API

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// UI Element
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoFilterInput = document.querySelector("#filter-input");
+const todoList = document.querySelector("#todo-list");
+const todoClearButton = document.querySelector("#clear-todos");
